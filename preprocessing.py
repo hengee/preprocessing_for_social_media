f61aae808a527a9d3f95a74d46ea050990028d15
Heng Ee (Angus) Tay <heng.tay@monash.edu>
2023-08-11 17:11:37 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -28,8 +28,28 @@ def remove_markdown(text):
     Returns:
         A string containing text with markdown and symbols removed.
     """
+    # Remove [removed]
+    text = re.sub(r'\[removed\]', '', text)
+
+    # Remove [deleted]
+    text = re.sub(r'\[deleted\]', '', text)
+
+    # Remove reddit /r/ tags but keep the tag content
+    text = re.sub(r'/r/(\w+)', r'\1', text)
+    
+
+    # Remove URLs, HTML tags
+    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r'www\S+', '', text)
+    text = re.sub(r'bit.ly\S+', '', text)
+    text = re.sub(r'bitly\S+', '', text)
+    text = re.sub(r'pic.twitter\S+', '', text)
+    text = re.sub(r'youtube.\S+', '', text)
+    text = re.sub(r'<.*?>', '', text) # remove html tags
+
     text = re.sub(r'^#{1,6}\s+', '', text) # Remove headings (e.g. ## Heading)
     text = re.sub(EMOJI_PATTERN, '', text) # Remove emoji
+    text = re.sub(r'\b\w{30,}\b', '', text) # remove words that more than 30 characters
     text = re.sub(r'\*\*(.*?)\*\*', r'\1', text) # Remove bold (e.g. **Bold**)
     text = re.sub(r'\*(.*?)\*', r'\1', text) # Remove italic (e.g. *Italic*)
     text = re.sub(r'^\>\s+', '', text, flags=re.MULTILINE) # Remove blockquote (e.g. > Quote)
@@ -42,16 +62,19 @@ def remove_markdown(text):
     text = re.sub(r'#(\S+)', r'\1', text) # Remove hashtag symbol (e.g. #hashtag)
     text = re.sub(r'\n', ' ', text) # Remove newline characters
     text = re.sub(r'\t', ' ', text) # Remove tab characters
-    # Remove URLs, HTML tags
-    text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'www\S+', '', text)
-    text = re.sub(r'bit.ly\S+', '', text)
-    text = re.sub(r'bitly\S+', '', text)
-    text = re.sub(r'pic.twitter\S+', '', text)
-    text = re.sub(r'youtube.\S+', '', text)
-    text = re.sub(r'<.*?>', '', text) # remove html tags
-    text = text.strip() # Remove leading and trailing whitespace
+	
+    # Remove words longer than 26 characters
+    text = re.sub(r'\b\w{26,}\b', '', text)
+
+    # Remove words with 3 or more consecutive characters (e.g. loooong -> loong)
+    text = re.sub(r'([a-z])\1{2,}', r'\1', text)
+    
+    # Remove leading and trailing whitespace
+    text = text.strip() 
     text = text.lower()
+    
+    # Remove extra whitespace
+    text = re.sub(r' +', ' ', text)
     return text
 
 
@@ -65,10 +88,16 @@ def remove_twitter(text):
     """
     # replace usernames with [user]
     text = re.sub(r'@\S+', '@user', text) # replace usernames with @user, @ and user exist in the vocab of sentence transformer
+    text = re.sub(r'\w*\d\w*', '', text) # remove words that contain numbers
     text = re.sub(AT_HASH_PATTERN,'',text) # remove @ and # symbols
     text = re.sub(RETWEET_PATTERN,'rt',text) # replace the word 'retweet' with 'rt', rt exist in the vocab of sentence transformer but not retweet
-
+    text = re.sub(r'&amp;', '', text) 
+    text = re.sub(r' +', ' ', text)
+    text = text.strip()
+    text = re.sub(r'\.\.\.', '', text)
     # since it has previously removed the emoji, we don't need to remove it again, but to play safe, we can encode it to utf-8
+    
+    #optional
     text = text.encode('utf-8', errors='ignore') # usually there's error involve with emoji, so we need to encode it to utf-8
 
     return text
